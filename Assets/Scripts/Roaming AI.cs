056681c18ecd5477b98472f2ddfecc60bb61318c
liviniapoo <s10255969@connect.np.edu.sg>
2024-08-02 19:11:20 +0800

    Setup for Roam AI code

---
+++ Assets/Scripts/Roaming AI.cs
@@ -0,0 +1,26 @@
+/*
+ * Author: Livinia Poo
+ * Date: 02/08/2024
+ * Description: 
+ * NPC/Quest Giver random roaming movement
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RoamingAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
