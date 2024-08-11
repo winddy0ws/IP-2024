08b35cb2c7239ff7dfae37b0a8aaeb833aee0027
liviniapoo <s10255969@connect.np.edu.sg>
2024-08-11 21:38:39 +0800

    audio manipulation in main menu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,12 +9,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
+    /// <summary>
+    /// Making the audio available in all scenes and scripts
+    /// </summary>
+    public static AudioManager instance;
+
+    /// <summary>
+    /// Variables
+    /// </summary>
     public Sound[] bgmSounds, sfxSounds;
     public AudioSource bgmSource, sfxSource;
 
+    /// <summary>
+    /// Function checks for BGM audio
+    /// </summary>
+    /// <param name="name"></param>
     public void PlayBGM(string name)
     {
         Sound sound = Array.Find(bgmSounds, x => x.soundName == name);
@@ -30,9 +43,13 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Function checks for SFX audio
+    /// </summary>
+    /// <param name="name"></param>
     public void PlaySFX(string name)
     {
-        Sound sound = Array.Find(bgmSounds, x => x.soundName == name);
+        Sound sound = Array.Find(sfxSounds, x => x.soundName == name);
 
         if (sound == null)
         {
@@ -46,6 +63,38 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        PlayBGM("Menu BGM");
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            PlayBGM("Menu BGM");
+        }
+        else
+        {
+            PlayBGM("Game BGM");
+        }
+    }
+
+    /// <summary>
+    /// Adjust music/sfx volume
+    /// </summary>
+    /// <param name="volume"></param>
+    public void BGMVolume(float volume)
+    {
+        Debug.Log($"Setting BGM volume to: {volume}");
+        bgmSource.volume = volume;
+    }
+    public void SFXVolume(float volume)
+    {
+
+        sfxSource.volume = volume;
     }
 }
--- Assets/Scripts/Main menu.cs
+++ Assets/Scripts/Main menu.cs
@@ -1,5 +1,5 @@
 /*
- * Author: Yau Wai Lam
+ * Author: Yau Wai Lam and Livinia Poo
  * Date: 24/07/24
  * Description: 
  * UI management for Main Menu
@@ -9,6 +9,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Mainmenu : MonoBehaviour
 {
@@ -16,6 +17,11 @@ public class Mainmenu : MonoBehaviour
     /// variable for which scene to navigate to
     /// </summary>
     public int Scene;
+
+    /// <summary>
+    /// Variables for music control
+    /// </summary>
+    public Slider _bgmSlider, _sfxSlider;
 
     /// <summary>
     /// Starts game with first scene after clicking Play
@@ -32,5 +38,41 @@ public class Mainmenu : MonoBehaviour
     {
         Application.Quit();
     }
-       
+
+    /// <summary>
+    /// Adjust music/sfx volume
+    /// </summary>
+    public void BGMVolume()
+    {
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+
+        if (AudioManager.instance != null)
+        {
+            audioManager.BGMVolume(_bgmSlider.value);
+        }
+        else
+        {
+            Debug.Log("AudioManager instance is null");
+        }
+    }
+    public void SFXVolume()
+    {
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+
+        if (AudioManager.instance != null)
+        {
+            audioManager.SFXVolume(_sfxSlider.value);
+        }
+
+    }
+
+    private void Start()
+    {
+        AudioManager instance = FindObjectOfType<AudioManager>();
+        if (instance != null)
+        {
+            Debug.Log("AudioManager found");
+        }
+        Debug.Log($"AudioManager instance in Mainmenu: {AudioManager.instance}");
+    }
 }
