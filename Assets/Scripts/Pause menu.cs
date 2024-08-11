789e036bd0f7a4644edf7f5aa4c737104128a04b
liviniapoo <s10255969@connect.np.edu.sg>
2024-08-11 21:49:52 +0800

    set up pause menu audio manipulation as well

--- Assets/Scripts/Pause menu.cs
+++ Assets/Scripts/Pause menu.cs
@@ -1,5 +1,5 @@
 /*
- * Author: Yau Wai Lam
+ * Author: Yau Wai Lam and Livinia Poo
  * Date: 24/07/24
  * Description: 
  * UI management and functions for pause menu
@@ -9,6 +9,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Pausemenu : MonoBehaviour
 {
@@ -16,6 +17,11 @@ public class Pausemenu : MonoBehaviour
     /// Variable for which scene to navigate to
     /// </summary>
     public int Scene;
+
+    /// <summary>
+    /// Variables for music control
+    /// </summary>
+    public Slider _bgmSlider, _sfxSlider;
 
     /// <summary>
     /// Determines whether the game is paused
@@ -29,6 +35,13 @@ public class Pausemenu : MonoBehaviour
     {
         Time.timeScale = 1f;
         Cursor.visible = false;
+
+        AudioManager instance = FindObjectOfType<AudioManager>();
+        if (instance != null)
+        {
+            Debug.Log("AudioManager found");
+        }
+        Debug.Log($"AudioManager instance in Mainmenu: {AudioManager.instance}");
     }
 
     // Update is called once per frame
@@ -87,5 +100,32 @@ public class Pausemenu : MonoBehaviour
     public void Quitgame()
     {
         Application.Quit();
+    }
+
+    /// <summary>
+    /// Adjust music/sfx volume
+    /// </summary>
+    public void BGMVolume()
+    {
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+
+        if (AudioManager.instance != null)
+        {
+            audioManager.BGMVolume(_bgmSlider.value);
+        }
+        else
+        {
+            Debug.Log("AudioManager instance is null");
+        }
+    }
+    public void SFXVolume()
+    {
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+
+        if (AudioManager.instance != null)
+        {
+            audioManager.SFXVolume(_sfxSlider.value);
+        }
+
     }
 }
