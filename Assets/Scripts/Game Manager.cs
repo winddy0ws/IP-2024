93bc8208e7990a33326f0e435fe1237067c9eb69
liviniapoo <s10255969@connect.np.edu.sg>
2024-08-16 00:23:38 +0800

    healthbar disappears and reappears yay

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour
 
     public float playerHealth = 100f;
     public GameObject HealthBar;
+    public GameObject HealthBarObj;
     public GameObject Die;
     public GameObject player;
 
@@ -32,26 +33,52 @@ public class GameManager : MonoBehaviour
     public static int bagCount = 0;
     public static int shroomCount = 0;
 
-  /*  public int Scene;
-    public int Level;*/
-
-    private void Start()
+    private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);  
         }
-        else if (Instance != this && Instance != null)
+        else if (Instance != this)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        UpdateHealthBarVisibility(SceneManager.GetActiveScene());
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     // Update is called once per frame
     void Update()
     { 
         Debug.Log("weeee");
-        HealthBar.GetComponent<Image>().fillAmount = playerHealth / 100;        
+        HealthBar.GetComponent<Image>().fillAmount = playerHealth / 100;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        UpdateHealthBarVisibility(scene);
+    }
+
+    private void UpdateHealthBarVisibility(Scene scene)
+    {
+        if (scene.buildIndex == 7)
+        {
+            HealthBarObj.SetActive(true);
+        }
+        else
+        {
+            HealthBarObj.SetActive(false);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
